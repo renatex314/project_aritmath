2c5a331117d29cde06d20cd5353085f95742790a
renatex314 <renatocorte34@gmail.com>
2025-05-25 16:45:03 -0300

    [UI and API separation] - 25/05/2025

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:3.13.3-bookworm
 
-RUN apt-get update && apt-get install ffmpeg libsm6 libxext6  -y
+RUN apt-get update && apt-get install ffmpeg libsm6 libxext6 nginx -y
 
 WORKDIR /usr/src/app
 
@@ -10,8 +10,13 @@ RUN pip install --no-cache-dir -r requirements.txt
 COPY model_downloader.py ./
 RUN python model_downloader.py
 
+COPY nginx.conf /etc/nginx/nginx.conf
 COPY . .
 
-EXPOSE 7860
-ENV GRADIO_SERVER_NAME="0.0.0.0"
-CMD ["python", "./main.py"]
+EXPOSE 80
+CMD [ \
+    "bash", "-c", \ 
+    "python", "/app/service1.py", "&", \
+    "python", "/app/service2.py", "&", \
+    "nginx", "-g", "'daemon off;'" \
+]
--- main.py
+++ main.py
@@ -1,152 +1,32 @@
 import gradio as gr
-import cv2
+import requests
 import numpy as np
-import sympy as sp
-from PIL import Image
-from transformers import TrOCRProcessor, VisionEncoderDecoderModel
-from sympy.parsing.latex import parse_latex
 
-processor = TrOCRProcessor.from_pretrained(
-    "fhswf/TrOCR_Math_handwritten",
-    cache_dir="./trocr_handwritten",
-    local_files_only=True,
-)
-model = VisionEncoderDecoderModel.from_pretrained(
-    "fhswf/TrOCR_Math_handwritten",
-    cache_dir="./trocr_handwritten",
-    local_files_only=True,
-).to("cpu")
+API_URL = "http://localhost:8001/predict_expression"
 
 
-def remover_ruido(imagem: np.ndarray) -> np.ndarray:
-    """
-    Remove o ruído (pequenos pontos) de uma imagem usando um filtro de mediana.
-    :param imagem: Imagem de entrada (numpy array).
-    :return: Imagem sem ruído (numpy array).
-    """
-    return cv2.medianBlur(imagem, 3)
-
-
-def corrigir_inclinacao(image: np.ndarray) -> np.ndarray:
-    """
-    Corrige a inclinação de uma imagem. (Deskew)
-    :param image: Imagem de entrada (numpy array).
-    :return: Imagem corrigida (numpy array).
-    """
-
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    gray = cv2.bitwise_not(gray)
-    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-
-    coords = np.column_stack(np.where(thresh > 0))
-    angle = cv2.minAreaRect(coords)[-1]
-
-    if angle < -45:
-        angle = -(90 + angle)
-    elif angle > 45:
-        angle = -(angle - 90)
-    else:
-        angle = -angle
-
-    (h, w) = image.shape[:2]
-    center = (w // 2, h // 2)
-
-    M = cv2.getRotationMatrix2D(center, angle, 1.0)
-    rotated = cv2.warpAffine(
-        image, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE
-    )
-
-    return rotated
-
-
-def aplicar_binarizacao(imagem: np.ndarray) -> np.ndarray:
-    """
-    Aplica binarização a uma imagem.
-    :param imagem: Imagem de entrada (numpy array).
-    :return: Imagem binarizada (numpy array).
-    """
-    gray = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
-    _, binarizada = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-
-    binarizada = cv2.cvtColor(binarizada, cv2.COLOR_GRAY2RGB)
-
-    return binarizada
-
-
-def evaluate_expression(expression):
-    try:
-        sympy_exp = parse_latex(expression)
-        result = sympy_exp.evalf()
-
-        return sympy_exp, result
-    except Exception as e:
-        return f"Erro: {str(e)}"
-
-
-def format_brazilian_number(number):
-    try:
-        number = float(number)
-        if number.is_integer():
-            return f"{int(number):,}".replace(",", ".")
-        else:
-            formatted = f"{number:,.2f}"
-            return formatted.replace(",", "v").replace(".", ",").replace("v", ".")
-    except:
-        return str(number)
-
-
-def format_detected_expression(expression: str):
-    expression = expression.split("=")[0]
-
-    return (
-        expression.replace("...", "")
-        .replace("X", "*")
-        .replace("x", "*")
-        .replace(" ", "")
-    )
-
-
-def predict_expression(source, sketch, uploaded_img):
+def predict_expression_ui(source, sketch, uploaded_img):
     # Escolhe a imagem correta
     image = sketch if source == "Desenhar" else uploaded_img
-    upload_type = "sketch" if source == "Desenhar" else "image"
-
     if image is None:
-        return "Nenhuma imagem fornecida."
+        return "Nenhuma imagem fornecida.", ""
 
     # Se for um dicionário (Sketchpad), extrai o array real
     if isinstance(image, dict) and "composite" in image:
         image = image["composite"]
-        image = np.array(image).astype(np.uint8)
 
-    if upload_type == "image":
-        image = remover_ruido(image)
-        image = corrigir_inclinacao(image)
-        image = aplicar_binarizacao(image)
+    # Converte a imagem para uma lista (JSON serializable)
+    if isinstance(image, np.ndarray):
+        image = image.tolist()
 
-    pil_image = Image.fromarray(image)
+    # Envia a imagem para a API de reconhecimento
+    response = requests.post(API_URL, json={"source": source, "image": image})
+    if response.status_code == 200:
+        data = response.json()
 
-    # TrOCR + avaliação
-    pixel_values = processor(images=pil_image, return_tensors="pt").pixel_values
-    generated_ids = model.generate(pixel_values)
-    original_expression = processor.batch_decode(
-        generated_ids, skip_special_tokens=True
-    )[0]
-
-    detected_expression = format_detected_expression(original_expression)
-
-    try:
-        results = evaluate_expression(detected_expression)
-        formatted_result = format_brazilian_number(results[1])
-
-        return f"$${detected_expression}$$", f"Resultado: {formatted_result}"
-    except Exception as err:
-        print(f"Ocorreu um erro ao processar a expressão: {err}")
-
-        return (
-            "",
-            "Ocorreu um erro ao processar a expressão. Verifique se a expressão está legível e formatada",
-        )
+        return f'$${data.get("expression", "")}$$', f'Resultado: {data.get("result", "")}'
+    else:
+        return "", "Erro ao processar a expressão."
 
 
 with gr.Blocks() as demo:
@@ -171,20 +51,13 @@ with gr.Blocks() as demo:
             type="numpy",
             interactive=True,
             visible=False,
-            value=Image.open("sample.png"),
         )
 
     # Atualiza a visibilidade dos componentes com base na seleção
     def update_source(source):
-        if source == "Desenhar":
-            sketch.visible = True
-            uploaded_img.visible = False
-        else:
-            sketch.visible = False
-            uploaded_img.visible = True
-
-        return gr.update(visible=sketch.visible), gr.update(
-            visible=uploaded_img.visible
+        return (
+            gr.update(visible=source == "Desenhar"),
+            gr.update(visible=source == "Imagem"),
         )
 
     source.change(
@@ -197,8 +70,8 @@ with gr.Blocks() as demo:
     outputs = [gr.Markdown(label="Expressão"), gr.Textbox(label="Resultado")]
 
     btn.click(
-        fn=predict_expression, inputs=[source, sketch, uploaded_img], outputs=outputs
+        fn=predict_expression_ui, inputs=[source, sketch, uploaded_img], outputs=outputs
     )
 
 if __name__ == "__main__":
-    demo.launch()
+    demo.launch(server_name="0.0.0.0", server_port=8000)
