9dd1bccfde0eacd80b697d6c2f0ff2fcd2f7a6eb
renatex314 <renatocorte34@gmail.com>
2025-06-01 23:59:45 -0300

    [update cors] - 01/06/2025

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import numpy as np
 import cv2
@@ -20,6 +21,13 @@ model = VisionEncoderDecoderModel.from_pretrained(
 
 # FastAPI app
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class ExpressionRequest(BaseModel):
