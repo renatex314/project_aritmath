822b3c7e88cbdcb15c0d7197d977e97dfaa2e1db
renatex314 <renatocorte34@gmail.com>
2025-06-02 00:44:18 -0300

    [privacy policy] - 02/06/2025 - Renato Corte

--- model_downloader.py
+++
@@ -1,9 +0,0 @@
-import os
-from huggingface_hub import snapshot_download
-
-if not os.path.exists("./trocr_handwritten"):
-    snapshot_download(
-        repo_id="fhswf/TrOCR_Math_handwritten", cache_dir="./trocr_handwritten"
-    )
-else:
-    print("Directory already exists. Skipping download.")
